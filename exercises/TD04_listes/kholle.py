411c6e3625a8a8a8a615f87f7aceb9d40515d745
Damien HARMANGE <damien.harmange@ens.uvsq.fr>
2021-11-10 18:47:44 +0100

    td4

---
+++ exercises/TD04_listes/kholle.py
@@ -0,0 +1,30 @@
+#TD noté
+#Exercice 1
+a = 10
+b = 20
+a = str(b) * 2
+b = a * 2
+print(a,b)
+"""
+#Exercice 2
+poids = 1000
+imperial = False
+if poids == kg:
+    else imperial = False:
+    print(imperial)
+if poids == pounds:
+    else imperial = True:
+    print(imperial)
+if poids >= 1000 kg or 2200 pounds:
+    else 
+
+#exercice 3 
+input("choisis un nombre")
+
+if nb /1 or nb / nb:
+    else "ce nombre est premier"
+if not nb / 1 or nb / nb:
+    else "ce nombre est pas premier, il est en particulier divisible par XXX"
+
+
+"""
